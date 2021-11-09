103b1ebf0594462425b4b9ecadd2fdecdb52812a
Shavkat Shavkiev <shavkatshavkiev2002@gmail.com>
2021-11-08 22:45:11 -0500

    Finished structure of site

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,14 +1,48 @@
-import React from 'react';
-import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
+import React from "react";
+import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from "react-icons/ai";
 
-import { SocialIcons } from '../Header/HeaderStyles';
-import { CompanyContainer, FooterWrapper, LinkColumn, LinkItem, LinkList, LinkTitle, Slogan, SocialContainer, SocialIconsContainer } from './FooterStyles';
+import { SocialIcons } from "../Header/HeaderStyles";
+import {
+  CompanyContainer,
+  FooterWrapper,
+  LinkColumn,
+  LinkItem,
+  LinkList,
+  LinkTitle,
+  Slogan,
+  SocialContainer,
+  SocialIconsContainer,
+} from "./FooterStyles";
 
 const Footer = () => {
   return (
-    <div>
-      Footer
-    </div>
+    <FooterWrapper>
+      <LinkList>
+        <LinkColumn>
+          <LinkTitle>Call</LinkTitle>
+          <LinkItem href="tel:646-988-0978">646-988-0978</LinkItem>
+        </LinkColumn>
+        <LinkColumn>
+          <LinkTitle>Email</LinkTitle>
+          <LinkItem href="mailto:shavkat.thedev@gmail.com">
+            Shavkat.thedev@gmail.com
+          </LinkItem>
+        </LinkColumn>
+      </LinkList>
+      <SocialIconsContainer>
+        <CompanyContainer>
+          <Slogan>Getting Better Every Day</Slogan>
+        </CompanyContainer>
+        <SocialContainer>
+          <SocialIcons href="https://Github.com">
+            <AiFillGithub size="3rem" />
+          </SocialIcons>
+          <SocialIcons href="https://LinkedIn.com">
+            <AiFillLinkedin size="3rem" />
+          </SocialIcons>
+        </SocialContainer>
+      </SocialIconsContainer>
+    </FooterWrapper>
   );
 };
 
--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -1,12 +1,47 @@
 import React from 'react';
-import { DiFirebase, DiReact, DiZend } from 'react-icons/di';
+import { DiFirebase, DiReact, DiZend, AiOutlineCloudServer } from 'react-icons/di';
+import { AiOutlineCloudSync } from "react-icons/ai";
 import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles/GlobalComponents';
 import { List, ListContainer, ListItem, ListParagraph, ListTitle } from './TechnologiesStyles';
 
 const Technologies = () =>  (
-  <div>
-    Technologies
-  </div>
+  <Section id='tech'>
+    <SectionDivider/>
+    <br />
+    <SectionTitle>Technologies</SectionTitle>
+    <SectionText>
+      PlaceHolder
+    </SectionText>
+    <List>
+      <ListItem>
+        <DiReact size='3rem' />
+        <ListTitle>Front-end</ListTitle>
+        <ListParagraph>
+          Experience With <br />
+          React
+        </ListParagraph>
+      </ListItem>
+      <ListItem>
+        <DiFirebase size='3rem' />
+        <ListTitle>Back-End</ListTitle>
+        <ListParagraph>
+          Experience With <br />
+          Node <br />
+          Express <br />
+          MySQL <br />
+          MongoDB <br />
+        </ListParagraph>
+      </ListItem>
+      <ListItem>
+        <AiOutlineCloudSync size='3rem' />
+        <ListTitle>Development Tools</ListTitle>
+        <ListParagraph>
+          Experience With <br />
+          AWS
+        </ListParagraph>
+      </ListItem>
+    </List>
+  </Section>
 );
 
 export default Technologies;
